1ad1c45dd64e8bedb0cd84d4ebebb31d27f841d5
cybertekschool <github@cybertekschool.com>
2020-02-06 15:23:59 -0500

    added properties file

--- src/test/java/com/bookit/runners/CucumberRunner.java
+++ src/test/java/com/bookit/runners/CucumberRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/bookit/step_definitions",
-        dryRun = true,
+        dryRun = false,
         tags = "@api and @create_student"
 )
 public class CucumberRunner {
