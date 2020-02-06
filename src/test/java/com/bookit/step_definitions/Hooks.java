74a8b9a896afc70142594b0e68cb738ef6bbf737
cybertekschool <github@cybertekschool.com>
2020-02-06 14:25:21 -0500

    added properties file

--- src/test/java/com/bookit/step_definitions/Hooks.java
+++ src/test/java/com/bookit/step_definitions/Hooks.java
@@ -1,4 +1,16 @@
 package com.bookit.step_definitions;
 
+import com.bookit.utilities.Environment;
+import io.cucumber.java.Before;
+import static io.restassured.RestAssured.*;
+
 public class Hooks {
+    /**
+     * This hook will work,
+     * only if scenario or feature has @api annotation
+     */
+    @Before("@api")
+    public void setupAPI(){
+        baseURI = Environment.BASE_URI;
+    }
 }
