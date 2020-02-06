6f3fd2eea37f42865f0db401b2b4e6b481536451
cybertekschool <github@cybertekschool.com>
2020-02-06 14:22:33 -0500

    added properties file

--- src/test/java/com/bookit/step_definitions/APIStepDefinitions.java
+++ src/test/java/com/bookit/step_definitions/APIStepDefinitions.java
@@ -4,14 +4,16 @@ import com.bookit.utilities.APIUtilities;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import static io.restassured.RestAssured.*;
 
 public class APIStepDefinitions {
     private Response response;
     private String token;
     private JsonPath jsonPath;
-    private String contentType;
+    private ContentType contentType;
 
     @Given("authorization token is provided for {string}")
     public void authorization_token_is_provided_for(String role) {
@@ -20,7 +22,11 @@ public class APIStepDefinitions {
 
     @Given("user accepts content type as {string}")
     public void user_accepts_content_type_as(String string) {
-
+        if(string.toLowerCase().contains("json")){
+            contentType = ContentType.JSON;
+        }else if(string.toLowerCase().contains("xml")){
+            contentType = ContentType.XML;
+        }
     }
 
     @When("user sends GET request to {string}")
---
+++ src/test/java/com/bookit/step_definitions/Hooks.java
@@ -0,0 +1,4 @@
+package com.bookit.step_definitions;
+
+public class Hooks {
+}
