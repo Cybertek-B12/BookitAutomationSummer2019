9aca075505d8437b47a5df3dda71a56245e50443
cybertekschool <github@cybertekschool.com>
2020-02-06 15:48:58 -0500

    added properties file

--- src/test/java/com/bookit/step_definitions/Hooks.java
+++ src/test/java/com/bookit/step_definitions/Hooks.java
@@ -1,5 +1,6 @@
 package com.bookit.step_definitions;
 
+import com.bookit.utilities.DBUtility;
 import com.bookit.utilities.Environment;
 import io.cucumber.java.*;
 import static io.restassured.RestAssured.*;
@@ -12,5 +13,15 @@ public class Hooks {
     @Before("@api")
     public void setupAPI(){
         baseURI = Environment.BASE_URI;
+    }
+
+    @Before("@db")
+    public void setupDB(){
+        DBUtility.createDBConnection(Environment.DB_HOST, Environment.DB_USERNAME, Environment.DB_PASSWORD);
+    }
+
+    @After("@db")
+    public void teardownDB(){
+        DBUtility.destroy();
     }
 }
