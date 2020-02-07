14ca285a31ad3e6b419eeaf4aee2c280bf9d4824
cybertekschool <github@cybertekschool.com>
2020-02-07 11:44:23 -0500

    added properties file

--- src/test/java/com/bookit/runners/CucumberRunner.java
+++ src/test/java/com/bookit/runners/CucumberRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/bookit/step_definitions",
         dryRun = false,
-        tags = "@print_room_names"
+        tags = " @verify_room_names"
 )
 public class CucumberRunner {
 }
--- src/test/java/com/bookit/step_definitions/APIStepDefinitions.java
+++ src/test/java/com/bookit/step_definitions/APIStepDefinitions.java
@@ -17,6 +17,7 @@ import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class APIStepDefinitions {
     private Response response;
@@ -31,9 +32,9 @@ public class APIStepDefinitions {
 
     @Given("user accepts content type as {string}")
     public void user_accepts_content_type_as(String string) {
-        if(string.toLowerCase().contains("json")){
+        if (string.toLowerCase().contains("json")) {
             contentType = ContentType.JSON;
-        }else if(string.toLowerCase().contains("xml")){
+        } else if (string.toLowerCase().contains("xml")) {
             contentType = ContentType.XML;
         }
     }
@@ -41,10 +42,10 @@ public class APIStepDefinitions {
     @When("user sends GET request to {string}")
     public void user_sends_GET_request_to(String string) {
         response = given().
-                        accept(contentType).
-                        auth().oauth2(token).
-                    when().
-                        get(string).prettyPeek();
+                accept(contentType).
+                auth().oauth2(token).
+                when().
+                get(string).prettyPeek();
     }
 
     //Then user should be able to see 18 rooms
@@ -59,6 +60,7 @@ public class APIStepDefinitions {
     public void user_verifies_that_response_status_code_is(int expected) {
         Assert.assertEquals(expected, response.statusCode());
     }
+
     /**
      * Any number in cucumber test step, becomes step definition  (variable)
      * By changing this number, you are not changing a context of test step
@@ -67,13 +69,13 @@ public class APIStepDefinitions {
 
     @When("user sends POST request to {string} with following information:")
     public void user_sends_POST_request_to_with_following_information(String path, List<Map<String, String>> students) {
-        for(Map<String, String> student: students){
+        for (Map<String, String> student : students) {
             response = given().
-                            auth().oauth2(token).
-                            queryParams(student).
-                            accept(contentType).
-                        when().
-                                post(path);
+                    auth().oauth2(token).
+                    queryParams(student).
+                    accept(contentType).
+                    when().
+                    post(path);
 
             response.then().log().body(true);
 
@@ -87,7 +89,7 @@ public class APIStepDefinitions {
 //        so we cannot change order
 //        Collections.sort(rooms);
 
-        for (Room room: rooms){
+        for (Room room : rooms) {
             System.out.println(room.getName());
         }
 
@@ -97,11 +99,16 @@ public class APIStepDefinitions {
 
         Collections.sort(rooms2);
 
-        for (Room room: rooms2){
+        for (Room room : rooms2) {
             System.out.println(room.getName());
         }
     }
 
+    @Then("user payload contains following room names:")
+    public void user_payload_contains_following_room_names(List<String> dataTable) {
+        List<String> actualRoomNames = response.jsonPath().getList("name");
+        Assert.assertTrue(actualRoomNames.containsAll(dataTable));
+    }
 
     @Then("user deletes previously added students")
     public void user_deletes_previously_added_students(List<Map<String, String>> students) {
